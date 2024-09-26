b708125a94dfabf891bb6ab85358ce4a8d7ce834
nnminh-haise <nnminh.sam.1803@gmail.com>
2024-09-26 16:00:00 +0700

    chore: remove console log and commented code in api response transformer

--- src/helper/mongoose/document-transofrmer.ts
+++ src/helper/mongoose/document-transofrmer.ts
@@ -3,20 +3,10 @@ export const MongooseDocumentTransformer: any = (doc: any) => {
 
   if ('id' in doc || !('_id' in doc)) return doc;
 
-  console.log('doc:', doc);
-
   let response = {
     id: doc._id.toString(),
     ...doc,
   };
   delete response._id;
   return response;
-
-  // const data = doc;
-  // const id = data._id.toString();
-  // delete data._id;
-  // return {
-  //   id: id,
-  //   ...data,
-  // };
 };
