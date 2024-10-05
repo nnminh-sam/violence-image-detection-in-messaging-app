f2098f606f6ace8afdb2eff5b48202c295a15214
nnminh-haise <nnminh.sam.1803@gmail.com>
2024-10-05 22:06:06 +0700

    chore: update repo

--- src/relationship/entities/Relationship.enum.ts
+++ src/relationship/entities/Relationship.enum.ts
@@ -4,6 +4,7 @@ enum RelationshipStatus {
   FRIENDS = 'FRIENDS',
   BLOCKED_USER_A = 'BLOCKED_USER_A',
   BLOCKED_USER_B = 'BLOCKED_USER_B',
+  AWAY = 'AWAY',
 }
 
 export default RelationshipStatus;
